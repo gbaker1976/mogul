07008e77670538bc92c5f2366faf9e3c5ca451a4
Garrett Baker <garrett.baker@exacttarget.com>
2014-05-23 23:08:17 -0400

    foo

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-var express = require("express");
-var logfmt = require("logfmt");
+var express = require( 'express' );
+var logfmt = require( 'logfmt' );
 var app = express();
 
-app.use(logfmt.requestLogger());
+app.use( logfmt.requestLogger() );
+//app.use( express.static( __dirname +  + '/' + pkg.version ) );
 
-app.get('/', function(req, res) {
-  res.send('Hello World!');
+app.get( '/', function( req, res ) {
+  res.send( 'Mogul' );
 });
 
-var port = Number(process.env.PORT || 5000);
-app.listen(port, function() {
-  console.log("Listening on " + port);
+var port = Number( process.env.PORT || 5000 );
+app.listen( port, function() {
+  console.log( 'Listening on ' + port );
 });
