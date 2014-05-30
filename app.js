ecda62f7050837f3eb9b05cfb839a7e1f8336418
Garrett Baker <garrett.baker@exacttarget.com>
2014-05-29 23:25:05 -0400

    reorganized files

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 var express = require( 'express' );
 var logfmt = require( 'logfmt' );
+var contextHandler = require( './lib/context-handler' );
+var pageHandler = require( './lib/page-handler' );
+var formHandler = require( './lib/form-handler' );
+var resourceHandler = require( './lib/resource-handler' );
 var hbs = require( 'hbs' );
-var fs = require( 'fs' );
 var app = express();
-var fieldsTemplatePath = __dirname + '/sites/fields.html';
-var fieldsTemplate = '';
 
 app.set( 'views', __dirname + '/sites' );
 app.engine( 'html', hbs.__express );
@@ -14,137 +15,18 @@ app.set( 'view engine', 'html' );
 app.use( logfmt.requestLogger() );
 
 // establish account context
-app.use( function( req, res, next ){
-	req.site = {
-		id: 'sweet_october',
-		pages: {
-			'/' : {
-				type: 'page',
-				template: 'index.html',
-				data: {
-					title: 'Sweet October: Intoxicating Scents for Lovers of the Dark',
-					content: '<header><h2>Intoxicating Scents for Lovers of the Dark</h2></header>',
-					quote: {
-						author: 'Mark Twain',
-						content: 'Everyone is a moon and has a dark side which he never shows to anybody.'
-					}
-				}
-			},
-			'/perfumes' : {
-				type: 'page',
-				template: 'left-sidebar.html',
-				data: {
-					title: 'Sweet October: Perfumes - Intoxicating Scents for Lovers of the Dark',
-					content: '<header><h2>Perfumes</h2><span class="byline">This is a byline.</span></header>',
-					quote: {
-						author: 'Henry Wadsworth Longfellow',
-						content: 'Into each life some rain must fall, somedays must be dark and dreary.'
-					}
-				}
-			},
-			'/about' : {
-				type: 'page',
-				template: 'right-sidebar.html',
-				data: {
-					title: 'Sweet October: About - Intoxicating Scents for Lovers of the Dark',
-					content: '<header><h2>About Sweet October</h2></header>',
-					quote: {
-						author: 'Mark Twain',
-						content: 'Everyone is a moon and has a dark side which he never shows to anybody.'
-					}
-				}
-			},
-			'/shop' : {
-				type: 'link',
-				data: 'http://sweetoctobershop.etsy.com/'
-			},
-			'/contact' : {
-				type: 'form',
-				template: 'form.html',
-				data: {
-					title: 'Sweet October: Contact - Intoxicating Scents for Lovers of the Dark',
-					content: '<header><h2>Contact</h2></header>',
-					quote: {
-						author: 'Henry Wadsworth Longfellow',
-						content: 'Into each life some rain must fall, somedays must be dark and dreary.'
-					}
-				},
-				fields: [{
-					textInput: true,
-					label: 'Full Name',
-					id: 'name',
-					name: 'name',
-					defaultValue: 'foo',
-					placeholder: 'please enter your name',
-					maxLength: 50
-				}],
-				processor: function( req, res ) {
-					res.send( 201, {
+app.use( contextHandler );
 
-					});
-				}
-			}
-		}
-	};
-
-	next();
-});
-
-// handle forms
-function renderFormHtml( page ){
-	var fields = page.fields;
-	var field = null;
-	var formHtml = '';
-
-	while( field = fields.shift() ) {
-		formHtml += hbs.handlebars.compile( fieldsTemplate )( field );
-	}
-
-	return formHtml;
-}
-
-app.post( '/form/:formId', function( req, res, next ){
-	var processor = null;
-
-	if ( req.site.forms ) {
-		if ( processor = req.site.forms[ req.param( 'formId' )] ) {
-			processor.call( {}, req, res );
-		}
-	}
-});
+//handle form posts
+app.use( '/form/:formId', formHandler );
 
 // serve static content
-app.get( /^\/js|css|images/, function( req, res, next ) {
-	var path = __dirname + '/sites/' + req.site.id + '/resources' + req.path;
-	res.sendfile( path );
-});
+app.use( '/js', resourceHandler );
+app.use( '/css', resourceHandler );
+app.use( '/images', resourceHandler );
 
 // handle dynamic pages
-app.get( /.*/, function( req, res, next ) {
-	
-	var page = req.site.pages[ req.path ];
-
-	if ( page ) {
-		switch ( page.type ) {
-			case 'link' :
-				res.location( page.data );
-				res.send( 302 );
-				return;
-			break;
-			case 'form' :
-				page.data.formHtml = renderFormHtml( page );
-			break;
-				
-		}
-
-		res.render( 
-			req.site.id + '/templates/' + page.template, 
-			page.data 
-		);
-	} else {
-		next();
-	}
-});
+app.get( /.*/, pageHandler );
 
 // handle 404
 app.use( function( req, res ) {
@@ -170,15 +52,8 @@ app.use( function( err, req, res ) {
 	);
 });
 
-var port = Number( process.env.PORT || 5000 );
-
-// fields template before starting the app;
-fs.readFile( fieldsTemplatePath, { encoding: 'utf-8' }, function( err, file ) {
-	if ( err ) throw err;
-
-	fieldsTemplate = file;
+var port = Number( process.env.PORT || 5000 );	
 	
-	app.listen( port, function() {
-		console.log( 'Listening on ' + port );
-	});
+app.listen( port, function() {
+	console.log( 'Listening on ' + port );
 });
