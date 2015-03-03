36cefe220ba29d8cab9ecb97c59bff58b113ce06
Garrett Baker <garrett.baker@exacttarget.com>
2015-03-03 10:01:07 -0500

    reworked postcss implementation to allow building of theme

--- gulpfile.js
+++ gulpfile.js
@@ -18,11 +18,58 @@ var copyFile = function( src, dest, cb ){
 gulp.task( 'css', function(){
 	return gulp.src( './src/css/_all.css' )
 	.pipe( cssnext({
-		compress: true
+		compress: false
 	}))
 	.pipe( postcss( [ postcssNested ] ) )
 	.pipe( gulp.dest( './dist/css' ) )
 });
+
+gulp.task( 'app-js', function(){
+	return gulp.src( './src/js/app.js' )
+	.pipe( gulp.dest( './dist/js' ) );
+});
+
+
+gulp.task( 'theme-css', function(){
+	return gulp.src( './src/themes/wasabi/src/css/theme.css' )
+	.pipe( cssnext({
+		compress: false
+	}))
+	.pipe( postcss( [ postcssNested ] ) )
+	.pipe( gulp.dest( './src/themes/wasabi/dist/css' ) )
+});
+
+gulp.task( 'theme-js', function(){
+	return gulp.src( './src/themes/wasabi/src/js/main.js' )
+	.pipe( gulp.dest( './src/themes/wasabi/dist/js' ) );
+});
+
+gulp.task(
+	'clean-themes',
+	function( cb ) {
+		fs.exists( './dist/theme', function( exists ){
+			if ( exists ) {
+				wrench.rmdirSyncRecursive( './dist/theme' );
+			}
+			fs.mkdir( './dist/theme', cb );
+		});
+	}
+);
+
+gulp.task(
+	'copy-theme-html',
+	function ( cb ) {
+		copyFile( './src/themes/wasabi/src/theme.html', './src/themes/wasabi/dist/theme.html', cb );
+	}
+);
+
+gulp.task(
+	'copy-themes',
+	function ( cb ) {
+		wrench.copyDirRecursive( './src/themes/wasabi/dist', './dist/theme/wasabi', { forceDelete: true }, cb );
+	}
+);
+
 
 gulp.task( 'app-js', function(){
 	return gulp.src( './src/js/app.js' )
@@ -38,13 +85,6 @@ gulp.task(
 );
 
 gulp.task(
-	'copy-themes',
-	function ( cb ) {
-		wrench.copyDirRecursive( './src/themes', './dist/themes', { forceDelete: true }, cb );
-	}
-);
-
-gulp.task(
 	'copy-html',
 	function ( cb ) {
 		copyFile( './src/index.html', './dist/index.html' );
@@ -55,16 +95,16 @@ gulp.task(
 gulp.task( 'serve', function() {
 	browserSync({
 		server: {
-			baseDir: './'
+			baseDir: 'dist'
 		}
 	});
 });
 
 gulp.watch( 'js/**/*.js', { cwd: 'src' },  [ 'app-js' ] );
 gulp.watch( 'css/**/*.css', { cwd: 'src' }, [ 'css' ] );
-gulp.watch( 'themes/**/*', { cwd: 'src' }, [ 'copy-themes' ] );
 gulp.watch( '*.html', { cwd: 'src' }, [ 'copy-html' ] );
 
 gulp.watch( [ 'js/**/*.js', 'css/**/*.css', '**/*.html' ], { cwd: 'dist' },  reload );
 
-gulp.task( 'default', [ 'clean-dist', 'css', 'app-js', 'copy-themes', 'copy-html', 'serve' ] );
+gulp.task( 'default', [ 'clean-dist', 'css', 'app-js', 'theme', 'copy-html', 'serve' ] );
+gulp.task( 'theme', [ 'clean-themes', 'theme-css', 'theme-js', 'copy-theme-html', 'copy-themes' ] );
---
+++ src/themes/wasabi/src/js/main.js
@@ -0,0 +1,27 @@
+
+(function( doc, win ){
+
+	var themeDoc = doc.currentScript.ownerDocument;
+
+	win.addEventListener( 'DOMContentLoaded', function(){
+		var svg = doc.createElementNS( 'http://www.w3.org/2000/svg', 'svg' );
+		var use = doc.createElementNS( 'http://www.w3.org/2000/svg', 'use' );
+
+		svg.setAttribute( 'class', 'icon' );
+		svg.appendChild( use );
+
+		// append our SVG sprite in the importing doc
+		doc.body.insertBefore( themeDoc.querySelector( '.savage-sprite' ).cloneNode( true ), doc.body.firstChild );
+
+		// replace images
+		[].forEach.call( doc.querySelectorAll( '[data-savage-icon]' ), function( el ){
+			use.setAttributeNS(
+				'http://www.w3.org/1999/xlink',
+				'href',
+				'#savage-' + el.getAttribute( 'data-savage-icon' )
+			);
+			el.parentNode.replaceChild( svg.cloneNode( true ), el );
+		});
+	});
+
+})( document, window );
