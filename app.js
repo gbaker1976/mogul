df23b5e1cb844901f32aa954fca3de058e41e881
Garrett Baker <garrett.baker@gmail.com>
2016-02-04 08:42:34 -0500

    added mecache

--- app.js
+++ app.js
@@ -5,10 +5,31 @@ var pageHandler = require( './lib/page-handler' );
 var hbs = require( 'hbs' );
 var app = express();
 var fs = require( 'fs' );
+var memjs = require( 'memjs' );
 
 app.set( 'views', process.cwd() + '/' );
 app.engine( 'html', hbs.__express );
 app.set( 'view engine', 'html' );
+
+var client = memjs.Client.create( process.env.MEMCACHEDCLOUD_SERVERS, {
+    username: process.env.MEMCACHEDCLOUD_USERNAME,
+    password: process.env.MEMCACHEDCLOUD_PASSWORD
+});
+
+client.get( 'test-data', function( err, value, key ){
+    if ( !value ) {
+        fs.readFile( path.join( process.cwd(), 'data.json' ), 'utf-8', function( err, contents ){
+            if ( err ) {
+                console.log( 'unable to read test data file' );
+                return;
+            }
+
+            if ( contents ) {
+                client.set( 'test-data', contents );
+            }
+        });
+    }
+});
 
 app.use( contextHandler );
 app.use( pageHandler );
