177eb8f3ae5b9136a6f07d16b99ba3bb9296e086
Garrett Baker <garrett.baker@gmail.com>
2016-02-03 22:45:24 -0500

    added alias resolution of pages and sites; removed static page service; changed data file load from require() to file read to allow data changes without reloading the app

--- app.js
+++ app.js
@@ -2,38 +2,16 @@ var express = require( 'express' );
 //var logfmt = require( 'logfmt' );
 var contextHandler = require( './lib/context-handler' );
 var pageHandler = require( './lib/page-handler' );
-var formHandler = require( './lib/form-handler' );
-var resourceHandler = require( './lib/resource-handler' );
 var hbs = require( 'hbs' );
 var app = express();
 var fs = require( 'fs' );
-var cwd = process.cwd();
-var path = require( 'path' );
-var paths = {
-    NotFound: path.join( cwd, '/sites/404.html' ),
-    Error: path.join( cwd, '/sites/500.html' )
-};
 
-app.set( 'views', __dirname + '/sites' );
+app.set( 'views', process.cwd() + '/' );
 app.engine( 'html', hbs.__express );
 app.set( 'view engine', 'html' );
 
-// serve application UI
-app.use( '/', function( req, res, next ){
-	fs.realpath( path.join( cwd, req.path ), function( err, path ){
-		if ( err ) {
-			res.status( 404 ).sendFile( paths.NotFound );
-            return;
-		}
-
-		res.sendFile( path, function( err ){
-            if ( err ) {
-                res.status( 404 ).sendFile( paths.NotFound );
-                return;
-            }
-        });
-	});
-});
+app.use( contextHandler );
+app.use( pageHandler );
 
 var port = Number( process.env.PORT || 5000 );
 
